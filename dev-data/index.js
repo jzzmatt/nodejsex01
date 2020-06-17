5b2b348517afda0e88fc33013753e256d3f9dfe8
Junior Mateus <mateusjunior.ns@gmail.com>
2020-06-17 08:31:49 +0100

    Last Mod

--- dev-data/index.js
+++ dev-data/index.js
@@ -56,12 +56,15 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
     // console.log(req.url)
     // res.end('Hello from the server!');
-    const pathName = req.url;
+    
+    const { query, pathname } = url.parse(req.url, true);
+    
+    //const pathName = req.url;
 
 
     // Overview Page
-    if (pathName === '/' || pathName === '/overview') {
-        res.writeHead(200, {'Contentype': 'text/html'});
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {'Content-type': 'text/html'});
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
@@ -70,11 +73,15 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
     // Product Page
-    } else if (pathName === '/product') {
-        res.end('This is the Product');
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+
+        res.end(output);
 
     // API
-    } else if (pathName === '/api'){
+    } else if (pathname === '/api'){
  
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data);
